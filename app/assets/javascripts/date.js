8f253b4c4ea99d43109519bf544acdca605c6331
PratikB-Cuelogic <pratik.bamb@Cuelogic.com>
2018-02-21 16:42:04 +0530

    F116 => Done

---
+++ app/assets/javascripts/date.js
@@ -0,0 +1,22 @@
+window.onload = function () {
+	
+	//Get html elements
+	var checkinSel = document.getElementById("search_checkinSel");	
+
+	
+	//City Changed
+	document.getElementById("search_checkinSel").onchange = function () {
+		var today = new Date(checkinSel.value);
+		var tomorrow = new Date(today);
+		tomorrow.setDate(today.getDate()+1);
+		var month = tomorrow.getUTCMonth() + 1; //months from 1-12
+		var day = tomorrow.getUTCDate();
+		var year = tomorrow.getUTCFullYear();
+		if( day < 10)
+		{day = '0'+ day}
+		if( month < 10)
+		{month = '0'+ month}	
+		var to = year + "-" + month + "-" + day;
+		document.getElementById("search_checkoutSel").setAttribute("min", to);
+	}
+}
