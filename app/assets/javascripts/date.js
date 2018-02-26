db4fa51cef74921f2bea533e9a777cf656a0d220
PratikB-Cuelogic <pratik.bamb@Cuelogic.com>
2018-02-26 11:02:11 +0530

    F116 => date.js and _index.html.erb changed

--- app/assets/javascripts/date.js
+++ app/assets/javascripts/date.js
@@ -1,19 +1,19 @@
-window.onload = function () {
-	
-	//Checkin Changed
-	document.getElementById("search_checkinSel").onchange = function () {
-		var today = new Date(checkinSel.value);
-		var tomorrow = new Date(today);
-		tomorrow.setDate(today.getDate()+1);
-		var month = tomorrow.getUTCMonth() + 1; //months from 1-12
-		var day = tomorrow.getUTCDate();
-		var year = tomorrow.getUTCFullYear();
-		if( day < 10)
-		{day = '0'+ day}
-		if( month < 10)
-		{month = '0'+ month}	
-		var to = year + "-" + month + "-" + day;
-		document.getElementById("search_checkoutSel").setAttribute("min", to);
-		document.getElementById('search_checkoutSel').value = to;
-	}
+//Checkin Changed
+function setCheckinCheckoutDate() {
+	checkinSel = document.getElementById("search_checkinSel").value;
+	console.log("22");
+	var today = new Date(checkinSel);
+	console.log(today);
+	var tomorrow = new Date(today);
+	tomorrow.setDate(today.getDate()+1);
+	var month = tomorrow.getUTCMonth() + 1; //months from 1-12
+	var day = tomorrow.getUTCDate();
+	var year = tomorrow.getUTCFullYear();
+	if( day < 10)
+	{day = '0'+ day}
+	if( month < 10)
+	{month = '0'+ month}	
+	var to = year + "-" + month + "-" + day;
+	document.getElementById("search_checkoutSel").setAttribute("min", to);
+	document.getElementById('search_checkoutSel').value = to;
 }
