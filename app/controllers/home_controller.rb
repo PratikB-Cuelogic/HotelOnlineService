81a18a2d888be187b1324d03fecedb6ca90c26bc
PratikB-Cuelogic <pratik.bamb@Cuelogic.com>
2018-02-27 19:40:35 +0530

    F116 => Updates Done

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def index
-  	@hotel_by_cities= Rails.cache.fetch('hotels'){Hotel.select("city").group(:city).limit(6)}
+  	@hotel_by_cities= Rails.cache.fetch('hotels'){Hotel.select("city").group(:city).limit(9)}
   end
 end
